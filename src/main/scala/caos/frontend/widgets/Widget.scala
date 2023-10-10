8f71f9ce45d47dc3a2ba7d9af9a4dd2b7993dff0
José Proença <jose@proenca.org>
2023-10-10 14:59:57 +0100

    changed help logo

--- src/main/scala/caos/frontend/widgets/Widget.scala
+++ src/main/scala/caos/frontend/widgets/Widget.scala
@@ -88,6 +88,8 @@ abstract class Widget[A](val title: String, doc: Documentation = Documentation()
         .style("background", "none")
         .style("box-shadow", "none")
         .style("padding", "3pt")
+        .style("margin-top","-16.6pt")
+
       if (title.nonEmpty) button.attr("title",title)
 
       drawButton(button,name)
@@ -117,6 +119,8 @@ abstract class Widget[A](val title: String, doc: Documentation = Documentation()
       case Right("refresh") =>
         button.append("span").attr("class", "glyphicon glyphicon-refresh")
       case Right("help") =>
+        Widget.helpSvg(button)
+      case Right("helpOld") =>
         val b = button.append("span")
         b .style("line-height","9pt")
           .style("padding","0pt 0pt 4pt 3pt")
@@ -247,6 +251,32 @@ object Widget {
       .attr("fill","none")
     svg.append("path")
       .attr("d","M 12 2 A 1 1 0 0 0 11.292969 2.2949219 L 6.1601562 7.1347656 A 0.5 0.5 0 0 0 6.1484375 7.1445312 L 6.1464844 7.1464844 A 0.5 0.5 0 0 0 6 7.5 A 0.5 0.5 0 0 0 6.5 8 L 10 8 L 10 16 C 10 16.552 10.448 17 11 17 L 12 17 L 13 17 C 13.552 17 14 16.552 14 16 L 14 8 L 17.5 8 A 0.5 0.5 0 0 0 18 7.5 A 0.5 0.5 0 0 0 17.853516 7.1464844 L 17.822266 7.1171875 L 12.716797 2.3027344 A 1 1 0 0 0 12.683594 2.2714844 A 1 1 0 0 0 12 2 z M 3 20 A 1.0001 1.0001 0 1 0 3 22 L 21 22 A 1.0001 1.0001 0 1 0 21 20 L 3 20 z")
+  }
+
+  def helpSvg(block: Block): Unit = {
+    val svg = block.append("svg")
+      .attr("xmlns", "http://www.w3.org/2000/svg")
+      .attr("width", "20")
+      .attr("height", "20")
+      .attr("viewBox", "0 0 24 24")
+      .attr("fill", "none")
+      .attr("class", "svgIcon")
+    svg.append("path")
+      .attr("d","M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z")
+      .attr("stroke","#b0b0b0")
+      .attr("stroke-width","2")
+    svg.append("path")
+      .attr("d", "M10.5 8.67709C10.8665 8.26188 11.4027 8 12 8C13.1046 8 14 8.89543 14 10C14 10.9337 13.3601 11.718 12.4949 11.9383C12.2273 12.0064 12 12.2239 12 12.5V12.5V13")
+      .attr("stroke", "#b0b0b0")
+      .attr("stroke-width", "2")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-linejoin", "round")
+    svg.append("path")
+      .attr("d", "M12 16H12.01")
+      .attr("stroke", "#b0b0b0")
+      .attr("stroke-width", "2")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-linejoin", "round")
   }
 
 
