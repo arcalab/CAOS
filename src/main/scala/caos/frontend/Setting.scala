1c4fc67c3442aca02c280f24def0d1a1a3e261bb
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2024-12-04 16:19:23 +0000

    overload and some cleanup

--- src/main/scala/caos/frontend/Setting.scala
+++ src/main/scala/caos/frontend/Setting.scala
@@ -24,27 +24,7 @@ case class Setting(name: String, children: List[Setting] = List(), var checked: 
     }
   }
 
-  private def getActive(path: String): List[String] = {
-    def collectPaths(setting: Setting, path: List[String]): List[String] = {
-      if (!setting.checked) Nil
-      else if (setting.children.isEmpty) List(path.mkString("."))
-      else setting.children.flatMap(child => collectPaths(child, path :+ child.name))
-    }
-
-    path2setting(path) match {
-      case Some(setting) => collectPaths(setting, List.empty)
-      case None => throw new RuntimeException(s"[$path] was not match on [$this]")
-    }
-  }
-
-  private def renderFromPath(path: String): Boolean = {
-    path2setting(path) match {
-      case Some(setting) => setting.checked
-      case _ => throw RuntimeException(s"[$path] was not matched on [$this]")
-    }
-  }
-
-  private def path2setting(path: String): Option[Setting] = {
+  private def path2setting(path: String): Setting = {
     @tailrec
     def resolvePath(currentSetting: Setting, remainingPath: List[String]): Option[Setting] = {
       remainingPath match
@@ -58,14 +38,16 @@ case class Setting(name: String, children: List[Setting] = List(), var checked: 
     }
 
     path.split("\\.").toList match {
-      case Nil => None
-      case head :: tail =>
-        if (this.name == head)
-          resolvePath(this, tail)
-        else
-          None
+      case Nil => throw RuntimeException(s"could not resolve [$path]")
+      case head :: tail if this.name == head =>
+          resolvePath(this, tail) match
+            case Some(setting) => setting
+            case _ => throw RuntimeException(s"could not match [$path] to a Setting")
+      case _ => throw RuntimeException(s"could not match [$path] to a Setting")
     }
   }
+
+  private def allFrom(path: String): Set[Setting] = allFrom(path2setting(path))
 
   private def allFrom(setting: Setting): Set[Setting] = {
     if (setting.children.isEmpty)
@@ -74,16 +56,22 @@ case class Setting(name: String, children: List[Setting] = List(), var checked: 
       Set(setting) ++ setting.children.flatMap(child => allFrom(child))
   }
 
+  private def allLeavesFrom(path: String): Set[Setting] = allLeavesFrom(path2setting(path))
+
   private def allLeavesFrom(setting: Setting): Set[Setting] = {
     allFrom(setting).filter(child => child.children.isEmpty)
   }
+
+  private def allActiveFrom(path: String): Set[Setting] = allActiveFrom(path2setting(path))
 
   private def allActiveFrom(setting: Setting): Set[Setting] = {
     allFrom(setting).filter(child => child.checked)
   }
 
-  // @ telmo - could be optimized through a filter, but I like the compositional behaviour
-  def allActiveLeavesFrom(setting: Setting): Set[Setting] = {
+  private def allActiveLeavesFrom(path: String): Set[Setting] = allActiveLeavesFrom(path2setting(path))
+
+  // @ telmo - could be optimized through ç a filter, but I like the compositional behaviour
+  private def allActiveLeavesFrom(setting: Setting): Set[Setting] = {
     allLeavesFrom(setting).intersect(allActiveFrom(setting))
   }
 
@@ -97,7 +85,9 @@ case class Setting(name: String, children: List[Setting] = List(), var checked: 
     toStringAuxiliary(this)
   }
 
-  def apply(path: String): List[String] = getActive(path)
+  def apply(path: String): Set[Setting] = apply(path2setting(path))
+
+  def apply(setting: Setting): Set[Setting] = allActiveLeavesFrom(setting)
 
   def unapply(setting: Setting): Option[(String, List[Setting], Boolean, List[String])] =
     Some((setting.name, setting.children, setting.checked, setting.options))
