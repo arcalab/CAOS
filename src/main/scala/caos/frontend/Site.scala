3bd7cf688ce90d2c6bdec82c87ea602abb1f465f
Telmo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2025-03-17 11:59:51 +0000

    Site cleanup && EW fix proposal

--- src/main/scala/caos/frontend/Site.scala
+++ src/main/scala/caos/frontend/Site.scala
@@ -11,37 +11,101 @@ import org.scalajs.dom.{FileReader, document, html}
 import scala.scalajs.js
 import scala.scalajs.js.annotation.*
 
-object Site:
-
-  private var leftColumn:DomElem = _
-  private var rightColumn:DomElem = _
-  private var errorArea:OutputArea = _
+object Site {
+  private var leftColumn: DomElem = _
+  private var rightColumn: DomElem = _
+  private var errorArea: OutputArea = _
   private var descriptionArea: OutputArea = _
-  private var toReload:List[()=>Unit] = _
+  private var toReload: List[() => Unit] = _
   private var lastConfig: Option[Configurator[_]] = None
 
-  private var codeWidget:     Option[CodeWidget[_]] = None
+  private var codeWidget: Option[CodeWidget[_]] = None
   private var examplesWidget: Option[ExampleWidget] = None
   private var mainExample: Option[Configurator.Example] = None
 
   private var settingWidget: Option[SettingWidget[_]] = None
 
-  def getSetting: Setting = settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined")).get
+  def getSetting: Setting = {
+    settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined")).get
+  }
 
-  def setSetting(setting: Setting): Unit =
-    val settingWidgetValue = settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined"))
-    settingWidgetValue.set(setting)
-    settingWidgetValue.update()
+  def setSetting(setting: Setting): Unit = {
+    val settingWidgetDefined = settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined"))
+    settingWidgetDefined.set(setting)
+    settingWidgetDefined.update()
     renderWidgets()
-  end setSetting
+  }
 
-  private def renderWidgets(): Unit =
+  def initSite[A](config: Configurator[A]): Unit = {
+    lastConfig = Some(config)
+    initialiseContainers()
+
+    // find the main example (from URL of first in the list)
+    val urlQuery = document.URL.split('?').drop(1).mkString("?")
+      .replaceAll("%2F", "/").replaceAll("%3C", "<")
+      .replaceAll("%26", "&").replaceAll("%3E", ">")
+      .replaceAll("%20", " ").replaceAll("%23", "#")
+      .replaceAll("%24", "$").replaceAll("%7B", "{")
+      .replaceAll("%2B", "+").replaceAll("%7D", "}")
+      .replaceAll("%2C", ",").replaceAll("%7C", "|")
+      .replaceAll("%3A", ":").replaceAll("%5E", "^")
+      .replaceAll("%3B", ";").replaceAll("%7E", "~")
+      .replaceAll("%3F", "?").replaceAll("%5B", "[")
+      .replaceAll("%40", "@").replaceAll("%5D", "]")
+      .replaceAll("%22", "\"").replaceAll("%60", "`")
+      .replaceAll("%28", "(").replaceAll("%29", ")")
+
+    mainExample = config.examples.find(_.name == urlQuery) match
+      case None =>
+        if urlQuery.nonEmpty
+        then Some(Configurator.Example(urlQuery, "Custom", ""))
+        else config.examples.headOption
+      case ex => ex
+
+    errorArea = new OutputArea
+    descriptionArea = new OutputArea
+
+    codeWidget = Some(mkCodeBox(config, mainExample))
+    codeWidget.getOrElse(throw RuntimeException("codeWidget is undefined")).init(leftColumn, true)
+
+    errorArea.init(leftColumn)
+
+    settingWidget = Some(mkSettingBox(config))
+    settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined")).init(leftColumn, true)
+
+    val title = document.getElementById("title")
+    title.textContent = config.name
+    val toolTitle = document.getElementById("tool-title")
+    toolTitle.textContent = config.name
+
+    examplesWidget = Some(
+      new ExampleWidget(
+        "Examples",
+        config,
+        globalReload(),
+        codeWidget.getOrElse(throw RuntimeException("codeWidget is undefined")),
+        Some(descriptionArea),
+        settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined"))
+      )
+    )
+
+    // place examples and information area
+    descriptionArea.init(leftColumn) // before the examples
+    examplesWidget.getOrElse(throw RuntimeException("examplesWidget is undefined")).init(leftColumn, true)
+
+    renderWidgets()
+
+    mainExample match
+      case Some(ex) => if (ex.description.nonEmpty) descriptionArea.setValue(ex.description)
+      case _ =>
+  }
+
+  private def renderWidgets(): Unit = {
     val config = lastConfig.getOrElse(throw RuntimeException("last config is undefined"))
     val code   = codeWidget.getOrElse(throw RuntimeException("codeWidget is undefined"))
 
     val widgets = (for (name, wi) <- config.smallWidgets yield (name, wi.moveTo(1))) ++ config.widgets
     val boxes = for wc <- widgets if wc._2.getRender yield
-      // build widget w
       val w = mkWidget(
         wc,
         () => code.get.asInstanceOf[config.StxType],
@@ -54,186 +118,131 @@ object Site:
       w
 
     toReload = (List(code) ++ boxes).map(b => () => b.update()) ++ List(settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined")).partialReload)
-  end renderWidgets
+  }
 
-  def initSite[A](config:Configurator[A]):Unit =
-    lastConfig = Some(config)
-    initialiseContainers()
-
-    // find the main example (from URL of first in the list)
-    val urlQuery = document.URL.split('?').drop(1).mkString("?")
-      .replaceAll("%2F", "/") .replaceAll("%3C", "<")
-      .replaceAll("%26", "&") .replaceAll("%3E", ">")
-      .replaceAll("%20", " ") .replaceAll("%23", "#")
-      .replaceAll("%24", "$") .replaceAll("%7B", "{")
-      .replaceAll("%2B", "+") .replaceAll("%7D", "}")
-      .replaceAll("%2C", ",") .replaceAll("%7C", "|")
-      .replaceAll("%3A", ":") .replaceAll("%5E", "^")
-      .replaceAll("%3B", ";") .replaceAll("%7E", "~")
-      .replaceAll("%3F", "?") .replaceAll("%5B", "[")
-      .replaceAll("%40", "@") .replaceAll("%5D", "]")
-      .replaceAll("%22", "\"").replaceAll("%60", "`")
-      .replaceAll("%28", "(") .replaceAll("%29", ")")
-
-    mainExample = config.examples.find(_.name == urlQuery) match
-      case None =>
-        if urlQuery.nonEmpty
-        then Some(Configurator.Example(urlQuery,"Custom",""))
-        else config.examples.headOption
-      case ex => ex
-
-    errorArea = new OutputArea
-    descriptionArea = new OutputArea
-
-    codeWidget = Some(mkCodeBox(config, mainExample))
-    codeWidget.getOrElse(throw RuntimeException("codeWidget is undefined")).init(leftColumn,true)
-
-    errorArea.init(leftColumn)
-
-    settingWidget = Some(mkSettingBox(config))
-    settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined")).init(leftColumn, true)
-
-    val title = document.getElementById("title")
-    val toolTitle = document.getElementById("tool-title")
-    title.textContent = config.name
-    toolTitle.textContent = config.name
-
-    examplesWidget = Some(new ExampleWidget("Examples",config,globalReload(),codeWidget.getOrElse(throw RuntimeException("codeWidget is undefined")),Some(descriptionArea),settingWidget.getOrElse(throw RuntimeException("settingWidget is undefined"))))
-
-    // place examples and information area
-    descriptionArea.init(leftColumn) // before the examples
-    examplesWidget.getOrElse(throw RuntimeException("examplesWidget is undefined")).init(leftColumn,true)
-
-    renderWidgets()
-
-    mainExample match
-      case Some(ex) => if (ex.description.nonEmpty) descriptionArea.setValue(ex.description)
-      case _ =>
   /**
    * Make widget box
-   * @param w widget info
+   *
+   * @param w   widget info
    * @param get function to get program
    * @param out output box to output errors
    * @tparam Stx Type of the program to process
    * @return a box
    */
-  private def mkWidget[Stx](w: (String,WidgetInfo[Stx]), get:()=>Stx,
-                              getAll:()=>Seq[(String,Stx)], out:OutputArea,
-                              doc: Documentation
-                             ): Widget[Unit] =
+  private def mkWidget[Stx](w: (String, WidgetInfo[Stx])
+                            , get: () => Stx
+                            , getAll: () => Seq[(String, Stx)]
+                            , out: OutputArea
+                            , doc: Documentation
+                           ): Widget[Unit] = {
     try w._2 match {
-      case Visualize(view,Mermaid,pre) => new VisualiseMermaid(()=>view(pre(get())),w._1,out,doc)
-      case Visualize(view,Text,pre) => new VisualiseText(()=>view(pre(get())),w._1,out,doc)
-      case Visualize(view,Code(lang),pre) => new VisualiseCode(()=>view(pre(get())),w._1,lang,out,doc)
-      case VisualizeAll(v,Mermaid,pre) => new VisualiseMermaid(()=>v(getAll().map(kv=>kv._1->pre(kv._2))),w._1,out,doc)
-      case VisualizeAll(v,Text,pre) => new VisualiseText(()=>v(getAll().map(kv=>kv._1->pre(kv._2))),w._1,out,doc)
-      case Visualize(_,Html,_) | VisualizeAll(_,Html,_) =>
+      case Visualize(view, Mermaid, pre) => new VisualiseMermaid(() => view(pre(get())), w._1, out, doc)
+      case Visualize(view, Text, pre) => new VisualiseText(() => view(pre(get())), w._1, out, doc)
+      case Visualize(view, Code(lang), pre) => new VisualiseCode(() => view(pre(get())), w._1, lang, out, doc)
+      case VisualizeAll(v, Mermaid, pre) => new VisualiseMermaid(() => v(getAll().map(kv => kv._1 -> pre(kv._2))), w._1, out, doc)
+      case VisualizeAll(v, Text, pre) => new VisualiseText(() => v(getAll().map(kv => kv._1 -> pre(kv._2))), w._1, out, doc)
+      case Visualize(_, Html, _) | VisualizeAll(_, Html, _) =>
         out.setValue("HTML visualiser not supported")
         sys.error("HTML visualiser not supported")
-      case VisualizeTab(views,Text,titles,pre) =>
-        new Tabs(()=>views(pre(get())),w._1,()=>titles(pre(get())),"",out,doc) // no language produces text boxes
+      case VisualizeTab(views, Text, titles, pre) =>
+        new Tabs(() => views(pre(get())), w._1, () => titles(pre(get())), "", out, doc) // no language produces text boxes
       case VisualizeTab(views, Code(lang), titles, pre) =>
-        new Tabs(() => views(pre(get())), w._1, () => titles(pre(get())), lang, out,doc)
-      case VisualizeOpt(view,t, pre) => t match {
-        case Mermaid => new VisualiseOptMermaid(()=>view(pre(get())),w._1,out,doc)
+        new Tabs(() => views(pre(get())), w._1, () => titles(pre(get())), lang, out, doc)
+      case VisualizeOpt(view, t, pre) => t match {
+        case Mermaid => new VisualiseOptMermaid(() => view(pre(get())), w._1, out, doc)
         case _ => throw new RuntimeException("case not covered...")
       }
       case sim@Simulate(_, _, _, t, _) => t match { // view(pre(get())) match {
-        case Text => new SimulateText(get,sim, w._1, out,doc)
-        case Mermaid => new SimulateMermaid(get,sim,w._1,out,doc)
+        case Text => new SimulateText(get, sim, w._1, out, doc)
+        case Mermaid => new SimulateMermaid(get, sim, w._1, out, doc)
         case _ => throw new RuntimeException(s"case not covered when compiling widget '${w._1}': $sim")
       }
-      case Explore(init,sos,vS,vA) => new widgets.Explore(()=>init(get()),sos,vS,vA,w._1,out,doc)
+      case Explore(init, sos, vS, vA) => new widgets.Explore(() => init(get()), sos, vS, vA, w._1, out, doc)
       case Analyse(a) =>
-        new Invisible[Stx,Unit](get, stx =>  (a(stx),Nil,()),w._1)
+        new Invisible[Stx, Unit](get, stx => (a(stx), Nil, ()), w._1)
       case _ => throw new RuntimeException(s"case not covered when compiling widget '${w._1}': ${w._2}")
     } catch {
       case e: Throwable =>
         out.error(e.getMessage)
         throw e
     }
+  }
 
-  private def cleanContainers():Unit = {
-    //    val contentDiv = DomNode.select("contentWrap")
-    //    contentDiv.deleteChildren
+  private def cleanContainers(): Unit = {
     val d = document.getElementById("contentWrap")
     val d_nested = document.getElementById("content")
     d.removeChild(d_nested)
   }
 
 
-  private def initialiseContainers():Unit =
+  private def initialiseContainers(): Unit = {
     val contentDiv = DomNode.select("contentWrap").append("div")
       .attr("class", "content")
       .attr("id", "content")
 
     val rowDiv = contentDiv.append("div")
-      //      .attr("class", "row")
       .attr("id", "mytable")
 
     leftColumn = rowDiv.append("div")
-      //      .attr("class", "col-sm-4")
       .attr("id", "leftbar")
       .attr("class", "leftside")
 
     leftColumn.append("div")
       .attr("id", "dragbar")
       .attr("class", "middlebar")
-//      .style("margin-left","24%")
 
     rightColumn = rowDiv.append("div")
-      //      .attr("class", "col-sm-8")
       .attr("id", "rightbar")
       .attr("class", "rightside")
 
     val overlay = contentDiv.append("div")
-      .attr("class","overlay")
-      .attr("id","CAOSOverlay")
+      .attr("class", "overlay")
+      .attr("id", "CAOSOverlay")
+
     val popup = contentDiv.append("div")
-      .attr("class","popup")
-      .attr("id","CAOSPopupWrp")
+      .attr("class", "popup")
+      .attr("id", "CAOSPopupWrp")
+
     val closePop = popup.append("div")
       .attr("class", "closePopup")
+
     popup.append("div")
-      .attr("id","CAOSPopup")
-    closePop.on("click",()=>{
-      dom.document.getElementById("CAOSOverlay").setAttribute("style","display:none;")
-      dom.document.getElementById("CAOSPopupWrp").setAttribute("style","display:none;")
+      .attr("id", "CAOSPopup")
+
+    closePop.on("click", () => {
+      dom.document.getElementById("CAOSOverlay").setAttribute("style", "display:none;")
+      dom.document.getElementById("CAOSPopupWrp").setAttribute("style", "display:none;")
     })
+
     overlay.on("click", () => {
       dom.document.getElementById("CAOSOverlay").setAttribute("style", "display:none;")
       dom.document.getElementById("CAOSPopupWrp").setAttribute("style", "display:none;")
     })
+
     closePop.append("div")
-      .attr("id","CAOSPopupTitle")
-      .attr("style","display:inline-block;font-weight: bold;")
-//      .html("title")
+      .attr("id", "CAOSPopupTitle")
+      .attr("style", "display:inline-block;font-weight: bold;")
+
     closePop.append("div")
-      .attr("style","float:right;")
+      .attr("style", "float:right;")
       .html("&#10006;")
 
-    if lastConfig.isDefined && lastConfig.get.footer!=""
+    if lastConfig.isDefined && lastConfig.get.footer != ""
     then contentDiv.append("div")
       .style("width: 100%;text-align: center; display: inline-block;")
       .html(s"&nbsp;<br><p style=\"margin: 0px 30px 10px;\">${lastConfig.get.footer}</p>")
 
-    //<div style="width: 100%;text-align: center; display: inline-block;">
-    //    &nbsp;<br>
-    //    <p style="margin: 0px 30px 10px;">Source code at: <a href="https://github.com/arcalab/choreo/tree/ceta" target="#">https://github.com/arcalab/choreo/tree/ceta</a>.
-    //    This is a companion tool for a paper under revision.
-    //    </p>
-    //</div>
-
     Utils.resizeCols
+  }
 
-
-  private def globalReload(): Unit =
+  private def globalReload(): Unit = {
     errorArea.clear()
-    toReload.foreach(f=>f())
+    toReload.foreach(f => f())
+  }
 
-  private def mkCodeBox[A](config:Configurator[A],
-                             ex:Option[Configurator.Example]):CodeWidget[A] =
-    new CodeWidget[A](config.languageName,Nil) {
+  private def mkCodeBox[A](config: Configurator[A]
+                           , ex: Option[Configurator.Example]): CodeWidget[A] = {
+    new CodeWidget[A](config.languageName, Nil) {
 
       protected var input: String = ex match
         case Some(e) => e.example
@@ -241,15 +250,21 @@ object Site:
 
       override protected val boxId: String = config.name + "Box"
 
-      override protected val buttons: List[(Either[String, String], (() => Unit, String))] =
+      override protected val buttons: List[(Either[String, String], (() => Unit, String))] = {
         List(
-          Right("refresh") -> (() => reload(), s"Load the ${config.languageName} program (shift-enter)"),
-          Right("download") -> (() => Utils.downloadTxt(ExampleWidget.examplesToTxt(
-            examplesWidget.getOrElse(throw RuntimeException("examplesWidget is undefined")).getCurrentExample
-          ), "examples.txt"),
-          "Download Current Example"))
-        :::
-          Widget.mkHelper(config.languageName,config.documentation).toList
+          Right("refresh") -> (
+            () => reload(),
+            s"Load the ${config.languageName} program (shift-enter)"
+          ),
+          Right("download") -> (
+            () => Utils.downloadTxt(ExampleWidget.examplesToTxt(
+              examplesWidget.getOrElse(throw RuntimeException("examplesWidget is undefined")).getCurrentExample),
+              "example.txt"
+            ),
+            "Download Current Example"
+          ),
+        ) ::: Widget.mkHelper(config.languageName, config.documentation).toList
+      }
 
       override def get: A = config.parser(input)
 
@@ -258,39 +273,37 @@ object Site:
       override def reload(): Unit =
         descriptionArea.clear()
         update()
-        //out.clear() // now already in globalReload()
         globalReload()
     }
+  }
 
-  private def mkSettingBox[A](config: Configurator[A]): SettingWidget[A] =
-    new SettingWidget[A]("Settings", Documentation(), config):
-      override protected val buttons: List[(Either[String, String], (() => Unit, String))] =
-        List(Right("refresh") -> (() => reload(), s"Load settings")) ::: Widget.mkHelper("settingBox",config.documentation).toList
+  private def mkSettingBox[A](config: Configurator[A]): SettingWidget[A] = {
+    new SettingWidget[A]("Settings", Documentation(), config) {
+      override protected val buttons: List[(Either[String, String], (() => Unit, String))] = {
+        List(
+          Right("refresh") -> (
+            () => reload(),
+            s"Load settings"
+          )
+        ) ::: Widget.mkHelper("settingBox", config.documentation).toList
+      }
 
-      override def partialReload(): Unit =
+      override def partialReload(): Unit = {
         errorArea.clear()
         update()
         document.getElementById("rightbar").innerHTML = ""
         renderWidgets()
-      end partialReload
+      }
 
-      override def reload(): Unit =
+      override def reload(): Unit = {
         descriptionArea.clear()
         errorArea.clear()
         update()
         document.getElementById("rightbar").innerHTML = ""
         renderWidgets()
-      end reload
-  end mkSettingBox
-
-//  @JSExportTopLevel("loadedFile")
-//  def loadedFile(ev: dom.UIEvent): Unit = {
-//    println("File Loaded Successfully...");
-//    var fileString = ev.target; // result
-//    println(fileString);
-////    $$("#op").text(fileString);
-////    appendPar(document.body, "You clicked the button!")
-//  }
+      }
+    }
+  }
 
   /** This method loads the examples from a local file. */
   @JSExportTopLevel("getFileAsText")
@@ -299,7 +312,6 @@ object Site:
     reader.readAsText(ev)
     reader.onload = _ => {
       val resultAsString = Utils.unfix(reader.result.toString)
-      //println("Loaded?")
       lastConfig match {
         case Some(c: Configurator[A] @unchecked) =>
           val c2 = new Configurator[A] {
@@ -311,12 +323,10 @@ object Site:
             override val examples: Iterable[Configurator.Example] =
               ExampleWidget.txtToExamples(resultAsString)
           }
-          //println("init site again")
           cleanContainers()
           initSite(c2)
         case _ =>
-          //println("no config...")
       }
-      //println(resultAsString)
     }
   }
+}
--- src/main/scala/caos/frontend/widgets/ExampleWidget.scala
+++ src/main/scala/caos/frontend/widgets/ExampleWidget.scala
@@ -7,17 +7,13 @@ import caos.frontend.Setting
 import caos.frontend.SettingParser
 import org.scalajs.dom
 import org.scalajs.dom.{DOMException, Event, FileReader, UIEvent}
-//import org.w3c.dom.DOMError
 
-//import scala.concurrent.{Future, Promise}
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
 
-/**
- * Created by   on 07/05/2021
- */
+
 class ExampleWidget(title:String
-                    , config:Configurator[_] // examples:Iterable[Example] //Seq[List[String]]
+                    , config:Configurator[_]
                     , reload: => Unit
                     , setableExample:Setable[String]
                     , setableDescription:Option[Setable[String]]=None
@@ -30,7 +26,7 @@ class ExampleWidget(title:String
 
   def getCurrentExample: Option[Example] = currentExample
 
-  override def get: Seq[(String,String)] = examples.map(e=>e.name->e.example).toSeq
+  override def get: Seq[(String, String)] = examples.map(e => e.name -> e.example).toSeq
 
   /**
    * Executed once at creation time, to append the content to the inside of this box
@@ -38,78 +34,28 @@ class ExampleWidget(title:String
    * @param div     Placeholder that will receive the "append" with the content of the box
    * @param visible is true when this box is initially visible (i.e., expanded).
    */
+
   override def init(div: Block, visible: Boolean): Unit = {
     val buttonsDiv = super.panelBox(div,visible,buttons=buttons).append("div")
       .attr("id", "buttons")
       .attr("style","padding: 2pt;")
-
-//    val inp = buttonsDiv.append("input")
-//      .attr("type","file")
-//      .attr("id","file")
-//      .attr("name","files[]")
-//
-//    import concurrent.ExecutionContext.Implicits.global
-//    def printFileContent(file: dom.File) =
-//      readTextFile(file).map {
-//        case Right(fileContent) => println(s"File content: $fileContent")
-//        case Left(error) => println(s"Could not read file ${file.name}. Error: $error")
-//      }
-//
-//    /** In the future, returns either the file's content or an error,
-//    if something went wrong */
-//    def readTextFile(fileToRead: dom.File): Future[Either[DOMException, String]] = {
-//
-//      // Used to create the Future containing either the file content or an error
-//      val promisedErrorOrContent = Promise[Either[DOMException, String]]
-//
-//      val reader = new FileReader()
-//      reader.readAsText(fileToRead, "UTF-8")
-//
-//      reader.onload = (_) => {
-//        val resultAsString = s"${reader.result}"
-//        promisedErrorOrContent.success(Right(resultAsString))
-//      }
-//      reader.onerror = (_: Event) => promisedErrorOrContent.success(Left(reader.error))
-//
-//      promisedErrorOrContent.future
-//    }
-////    val reader = new dom.FileReader()
-////    reader.readAsText(e.currentTarget.files.item(0))
-////    reader.onload(
-////    scalajs.js.eval(
-////      """function startRead(evt) {
-////        |    var file = document.getElementById('file').files[0];
-////        |    if (file) {
-////        |        //  getAsText(file);
-////        |        alert("Name: " + file.name + "\n" + "Last Modified Date :" + file.lastModifiedDate);
-////        |    }
-////        |}""".stripMargin
-////    )
-
-    buttonsDiv
       .style("display:block; padding:2pt")
 
-    for (ex <- examples ) yield genButton(ex,buttonsDiv)
+    for (ex <- examples ) yield genButton(ex, buttonsDiv)
   }
 
-//  @JSExportTopLevel("loadedFile")
-//  def loadedFile(ev: dom.Event): Unit = {
-//    println("File Loaded Successfully");
-//    var fileString = ev.target.toString;
-//    println(fileString);
-////    $$("#op").text(fileString);
-////    appendPar(document.body, "You clicked the button!")
-//  }
-
-  private def buttons = List(
-    Right("upload")-> (
-      () => Utils.uploadTxt() , //Utils.downloadTxt(s"examples: ${examples.map(_.name).mkString(", ")}", "examples.txt"),
-      "Upload Examples"),
-    Right("download")-> (
-      () => Utils.downloadTxt(ExampleWidget.examplesToTxt(examples),"examples.txt"),
-      "Download Examples")
-  ) ++
-    Widget.mkHelper(title,config.documentation)
+  private def buttons: List[(Either[String, String], (() => Unit, String))] = {
+    List(
+      Right("upload") -> (
+        () => Utils.uploadTxt(),
+        "Upload Examples"
+      ),
+      Right("download") -> (
+        () => Utils.downloadTxt(ExampleWidget.examplesToTxt(examples), "examples.txt"),
+        "Download Examples"
+      ),
+    ) ++ Widget.mkHelper(title, config.documentation)
+  }
 
   /**
    * Block of code that should read the dependencies and:
@@ -118,7 +64,7 @@ class ExampleWidget(title:String
    */
   override def update(): Unit = ()
 
-  protected def genButton(ex:Example,buttonsDiv:Block): Unit = {
+  private def genButton(ex:Example,buttonsDiv:Block): Unit = {
     val button = buttonsDiv.append("button").textEl(ex.name)
     button.on("click",() => {
       currentExample = Some(ex)
@@ -160,36 +106,42 @@ object ExampleWidget {
   def updateExamples(): Unit = {
     val bt = dom.document.getElementById("buttons")
     val btt = DomNode(bt)
-//    btt.append()
   }
 
   def txtToExamples(str:String): Iterable[Example] = {
     val list = str.split("module *")
       for (ex <- list if ex != "") yield {
         try {
-          val (name, rest) = unfix(ex).span(_ != ':')
+          val (name, rest) = ex.span(_ != ':')
           val rest2 = rest.drop(18) // drop "description"
           val (desc, rest3) = rest2.span(_ != '\n')
           val (code, rest4) = rest3.span(_ != '@')
           val setting = rest4.tail
-          Example(code.trim, name.trim, desc.trim, Some(SettingParser.parseSetting(setting.trim)))
+          Example(unfix(code.trim), unfix(name.trim), unfix(desc.trim), Some(SettingParser.parseSetting(unfix(setting.trim))))
         } catch {
           case e:Throwable => throw new RuntimeException(s"Failed to import when reading: $ex")
         }
       }
   }
 
-  def examplesToTxt(examples:Iterable[Example]): String =
-    examples.map(e => s"module ${e.name}:\\n// description: ${
-      fix(e.description)}\\n${fix(e.example)}\\n@${fix(e.setting.getOrElse(Setting()).toStringRaw)}").mkString("\\n\\n")
+  def examplesToTxt(examples: Iterable[Example]): String = {
+    examples.map(example =>
+      s"module ${example.name}:\\n" +
+        s"// description: ${fix(example.description)}\\n" +
+        s"${fix(example.example)}\\n" +
+        s"@${fix(example.setting.getOrElse(Setting()).toStringRaw)}")
+      .mkString("\\n\\n")
+  }
 
-  private def fix(s:String): String = s
-      .replaceAll("\\\\n","§NL;") // replaced in UTILS
-//      .replaceAll("\"","\\\"")
+  private def fix(unfixedString: String): String = {
+    unfixedString
       .replaceAll("module","§MODL;")
+      .replaceAll("@","§AT;")
+  }
 
-  private def unfix(s:String): String = s
-//    .replaceAll("\\\\n","\\n")
-//    .replaceAll("\\\"","\"")
-    .replaceAll("§MODL;","module")
+  private def unfix(fixedString: String): String = {
+    fixedString
+      .replaceAll("§MODL;","module")
+      .replaceAll("§AT;","@")
+  }
 }
