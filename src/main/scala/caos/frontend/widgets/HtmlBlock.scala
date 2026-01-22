bc19a086c251c94469fa9ee4a275814f732afcdd
José Proença <jose@proenca.org>
2026-01-22 11:26:24 +0000

    added support for HTML blocks

---
+++ src/main/scala/caos/frontend/widgets/HtmlBlock.scala
@@ -0,0 +1,32 @@
+package caos.frontend.widgets
+
+class HtmlBlock(block:String, title: String) extends Widget[Unit](title):
+  //val content = new OutputArea
+
+  override def get: Unit = {}
+
+  /**
+   * Executed once at creation time, to append the content to the inside of this box
+   *
+   * @param div     Placeholder that will receive the "append" with the content of the box
+   * @param visible ignored, since this box will never be hidden.
+   */
+  override def init(div: Block, visible: Boolean): Unit =
+    div.append("div").html(block)
+//    content.outputs.style("padding: 0px 5px 0px 5px;")
+//    update()
+
+  /**
+   * Block of code that should read the dependencies and:
+   *  - update its output value, and
+   *  - produce side-effects (e.g., redraw a diagram)
+   */
+  override def update(): Unit = {}
+
+  override protected def panelBox(parent:Block,
+                         visible:Boolean,
+                         headerStyle: List[(String,String)] = Nil,
+                         buttons:List[(Either[String,String], (()=>Unit,String) )] = Nil) : Block =
+    parent
+
+
